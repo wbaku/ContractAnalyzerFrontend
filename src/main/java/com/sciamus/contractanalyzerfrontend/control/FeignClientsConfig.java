60d5acef79a0093d70b0ceb90fe3aab216acbf83
Wojtek Bączkowski <wojciech.baczkowski@sciamus.com>
2021-05-17 12:14:01 +0200

    integration comlpeted

--- src/main/java/com/sciamus/contractanalyzerfrontend/control/FeignClientsConfig.java
+++ src/main/java/com/sciamus/contractanalyzerfrontend/control/FeignClientsConfig.java
@@ -2,7 +2,6 @@ package com.sciamus.contractanalyzerfrontend.control;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import lombok.RequiredArgsConstructor;
 import org.keycloak.KeycloakSecurityContext;
 import org.keycloak.adapters.RefreshableKeycloakSecurityContext;
 import org.springframework.context.annotation.Bean;
@@ -16,25 +15,27 @@ public class FeignClientsConfig {
         return new KeycloakRequestInterceptor(keycloakSecurityContext);
     }
 
-    @RequiredArgsConstructor
     static class KeycloakRequestInterceptor implements RequestInterceptor {
 
 
+        public KeycloakRequestInterceptor(KeycloakSecurityContext keycloakSecurityContext) {
+            this.keycloakSecurityContext = keycloakSecurityContext;
+        }
+
         private final KeycloakSecurityContext keycloakSecurityContext;
+
 
         @Override
         public void apply(RequestTemplate template) {
 
             ensureTokenIsStillValid();
 
-            // We use the Access-Token of the current user to call the service
-            // Authorization: Bearer
-            // eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJMT0Rx....
 
             template.header(HttpHeaders.AUTHORIZATION, "Bearer " + keycloakSecurityContext.getTokenString());
 
             System.out.println("JESTEM TUTAJ " + keycloakSecurityContext.getTokenString());
         }
+
         private void ensureTokenIsStillValid() {
             if (keycloakSecurityContext instanceof RefreshableKeycloakSecurityContext) {
                 RefreshableKeycloakSecurityContext.class.cast(keycloakSecurityContext).refreshExpiredToken(true);
